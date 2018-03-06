5a96c5de3c81b365f66c075fd71f7a4da20d2179
Davin Kelly <davinkelly@overstock.com>
2018-03-06 09:03:25 +0000

    Removing node modules

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import FontIcon from 'material-ui/FontIcon';
-import {blue500, red500, greenA200} from 'material-ui/styles/colors';
-
-import planeComponent from './planeComponent.js';
+import ButtonComponent from './ButtonComponent';
 import picRock from './pulpitrock.jpg'
+// import FontIcon from 'material-ui/FontIcon';
+// import {blue500, red500, greenA200} from 'material-ui/styles/colors';
 
 class App extends Component {
 
 
+    render() {
+        const style = {
+            margin: 12,
+        };
+        // const iconStyles = {
+        //   marginRight: 24,
+        // };
 
-  render() {
-    const style = {
-    margin: 12,
-    };
-    const iconStyles = {
-      marginRight: 24,
-    };
+        return (
+            <div className="App">
+                <MuiThemeProvider>
+                    <div>
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <h1 className="App-title">Welcome to React</h1>
+                        </header>
+                        <p className="App-intro">
+                            To get started, edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <RaisedButton label="Primary" primary={true} style={style}/>
 
-    return (
-      <div className="App">
-        <MuiThemeProvider>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-              <RaisedButton label="Primary" primary={true} style={style} />
+                        <ButtonComponent/>
 
-              <planeComponent />
+                        <img src={picRock} width="100" height="50" alt={"dddd"}/>
 
-                <img src={picRock} width="100" height="50" />
+                        Welcome to Product Portal
 
-               Welcome to Product Portal
+                        <RaisedButton label="ADD" primary={true} style={style}/>
 
-               <RaisedButton label="ADD" primary={true} style={style} />
-
-              <RaisedButton label="DELETE" primary={true} style={style} />
-
-            </MuiThemeProvider>
-      </div>
-    );
-  }
+                        <RaisedButton label="DELETE" primary={true} style={style}/>
+                    </div>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
 }
 
 export default App;
